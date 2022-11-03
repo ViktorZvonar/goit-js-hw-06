1938c8404223c2d8fcbc61a100ab09c358effa42
ViktorZvonar <viktorzvonar@yahoo.com>
2022-11-03 21:00:14 +0200

    8 and 9 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const formElements = event.currentTarget.elements;
+
+  const mail = formElements.email.value;
+  const password = formElements.password.value;
+
+  if (mail === "" || password === "") {
+    alert("Please fill in all the fields!");
+  } else {
+    const allData = {
+      mail,
+      password,
+    };
+    console.log(allData);
+  }
+
+  event.currentTarget.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonEl = document.querySelector(".change-color");
+const colorEl = document.querySelector(".color");
+
+const changeColor = () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  colorEl.textContent = getRandomHexColor();
+};
+
+buttonEl.addEventListener("click", changeColor);
